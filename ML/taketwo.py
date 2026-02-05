ecf3e14e3f8b8e95a2647bd862e7374d6f5dd5c0
shanegcstearns <shanestearns2004@gmail.com>
2026-02-04 23:08:47 -0800

    updating documentation

--- ML/taketwo.py
+++ ML/taketwo.py
@@ -1,109 +1,73 @@
+# taketwo.py
+# Python ML component for the sensing SOC group in CSE 127A 
+#
+# This is our best of many attempts at building an ML model to predict sleep
+# stages based on sensor data. The main features we are using from the current dataset
+# are mody movement, cosine, and heart rate.
+#
+# Built by Jackson Friday and Shane Stearns
+# Updated 2/4/2026
+# 
+# We obtained our train and test data from:
+# https://physionet.org/content/sleep-accel/1.0.0/
+# https://academic.oup.com/sleep/article/42/12/zsz180/5549536
+
+# Libraries
 import torch
-# import matplotlib.pyplot as plt
-from sklearn.datasets import make_moons
+from sklearn.datasets import make_moons 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
 from torch.utils.data import TensorDataset, DataLoader, WeightedRandomSampler
 import torch.nn as nn
-
 import pandas as pd
 import numpy as np
-# ============================================================
-# Data Features:
-# 1. Accel magnitude
-# 2. Accel magnitude slope (jerk)
-# 3. Heart rate delta
-# 4. HR RMSSD (if possible)
-# 5. Skin temperature?
-# 6. HRV / movement (possibly)
-#
-# Want to identify: after X amount of time has elapsed, can we reasonably predict when is a good time to wake up user
-# ============================================================
 
-SETUP_MAX_SAMPLES = 10 * 60 * 1   # first 10 minutes (adjust FS if needed)
-MIN_SETUP_SAMPLES = 0           # minimum Wake samples to trust
-EPS = 1e-8
-ACCEL_SHIFT = 6  # for accel baseline calc, bits to shift by.
+SETUP_MAX_SAMPLES = 10 * 60 * 1        # Using the first 10 minutes of data to get a baseline HR
+MIN_SETUP_SAMPLES = 0                  
+EPS = 1e-8                             
+ACCEL_SHIFT = 6                        # Used to scale and normalize accel data
 
-#History window size, for model input and RMSSD
-WINDOW_SEC = 60  # 1-minute window
-STEP_SEC = 300   # every 5 minutes
-WINDOW_SAMPLES = WINDOW_SEC 
-STEP_SAMPLES = STEP_SEC
+WINDOW_SEC = 60                        # Size of sliding window (in samples/seconds) for HRV/RMSSD calculation
+STEP_SEC = 300                         # Compute every 5 minutes
+WINDOW_SAMPLES = WINDOW_SEC            
+STEP_SAMPLES = STEP_SEC                
 
-SEED = 16 #42 org
-EPOCHS = 50
-LR = 1e-3
+SEED = 16                              # Random seed
+EPOCHS = 50                            # How many times the data is passed through the model
+LR = 1e-3                              # Learning rate, or how much the model updates weights each time
 
+
+# Load & clean up data from CSVs
 df = pd.read_csv("compiled_sleep_dataset.csv")
-
 need = ["subject_id", "movement", "cosine","time", "hr", "ss"]
 df = df.dropna(subset=need).copy()
 
-#Intuition: the model will be slept for X amount of hours,
-# we (probably) won't be in wake, even if we are it gets miscategorized as light sleep
 
-# ============================================================
-# HR baseline + delta
-# ============================================================
+# each subject's heart rate baseline
 resting_hr = {}
-# baseline_x = np.zeros(len(df))
-# baseline_y = np.zeros(len(df))
-# baseline_z = np.zeros(len(df))
 
 for sid, g in df.groupby("subject_id", sort=False):
-    idx = g.index  # indices in the full df
-    g = g.reset_index(drop=True)  # optional, for clean local indexing
-
-    # HR baseline
+    idx = g.index
+    g = g.reset_index(drop=True)
     early = g.iloc[:SETUP_MAX_SAMPLES]
     wake_hr = early.loc[early["ss"] == "W", "hr"].values
     if len(wake_hr) >= MIN_SETUP_SAMPLES:
         resting_hr[sid] = np.median(wake_hr)
     else:
         resting_hr[sid] = early["hr"].mean()
-
-    # Accel EMA baseline
-    # b_x = df.loc[idx[0], "x"]
-    # b_y = df.loc[idx[0], "y"]
-    # b_z = df.loc[idx[0], "z"]
-
-    # for j, i in enumerate(idx):
-    #     b_x += (df.loc[i, "x"] - b_x) / (2**ACCEL_SHIFT)
-    #     b_y += (df.loc[i, "y"] - b_y) / (2**ACCEL_SHIFT)
-    #     b_z += (df.loc[i, "z"] - b_z) / (2**ACCEL_SHIFT)
-
-    #     baseline_x[i] = b_x
-    #     baseline_y[i] = b_y
-    #     baseline_z[i] = b_z
-
-# Subtract the baselines
-# df["x_c"] = df["x"] - baseline_x
-# df["y_c"] = df["y"] - baseline_y
-# df["z_c"] = df["z"] - baseline_z
-
-# HR baseline & delta
 df["hr_baseline"] = df["subject_id"].map(resting_hr)
-df["delta_hr"] = (df["hr"] - df["hr_baseline"]) / (df["hr_baseline"] + EPS) #what does EPS do here?
+df["delta_hr"] = (df["hr"] - df["hr_baseline"]) / (df["hr_baseline"] + EPS)
 
-# ============================================================
-# Label mapping + Clearing out Wake
-# ============================================================
-df = df[~df['ss'].isin(['W'])]
-label_map = {"N1": 0, "N2": 0, "R": 0,"N3": 1} # 0 = Okay to wake, 1 = bad to wake
+df = df[~df['ss'].isin(['W'])] # removing wake stages
+
+# mapping sleep stages
+label_map = {"N1": 0, "N2": 0, "R": 0,"N3": 1}
 df = df[df["ss"].isin(label_map)]
 df["label"] = df["ss"].map(label_map).astype(int)
 
-# ============================================================
-# Feature Creation
-# ============================================================
-# # Magnitude from centered axes
-# df["accel_mag"] = np.sqrt(df["x_c"]**2 + df["y_c"]**2 + df["z_c"]**2) 
-# # Jerk
-# df["accel_jerk"] = np.abs(df["accel_mag"].diff().fillna(0)) #trying abs, having negatives makes it more complex
 
-# HR RMSSD
+# hrv
 hr = df["hr"].to_numpy()
 rmssd = np.zeros(len(hr))
 
@@ -112,32 +76,26 @@ for start in range(0, len(hr) - WINDOW_SAMPLES + 1, STEP_SAMPLES):
     hr_window = hr[start:end]
     hr_diff = np.diff(hr_window)
     rmssd_value = np.sqrt(np.mean(hr_diff**2))
-    
-    # assign this value to all samples in the window
     rmssd[start:end] = rmssd_value
 df["hr_rmssd"] = rmssd
 
-#Composite HRV + movement feature
-df["hrv_mov"] = df["hr_rmssd"] / (df["movement"] + EPS) #try out composite movement + HRV feature 
+# combining hrv and movement
+df["hrv_mov"] = df["hr_rmssd"] / (df["movement"] + EPS)
 
 
 features = [
-    "movement",
-    "cosine",
-    "delta_hr",
-    "hr_rmssd",
-    #"temp",
-    "hrv_mov"
+    "movement",    # body movement magnitude from accel
+    "cosine",      # circadian/time-of-day signal
+    "delta_hr",    # change in heartrate from baseline
+    "hr_rmssd",    # hrv
+    "hrv_mov"      # hrv and movement
 ]
 
 X = df[features].fillna(0).to_numpy()
 y = df["label"].to_numpy()
 
-# ============================================================
-# Train / test split
-# ============================================================
-
-np.random.seed(SEED)  # reproducible
+# train / test split by ID
+np.random.seed(SEED)
 subject_ids = df["subject_id"].unique()
 print(subject_ids)
 train_subs = np.random.choice(subject_ids, size=22, replace=False)
@@ -151,14 +109,11 @@ test_df = df[df["subject_id"].isin(test_subs)]
 
 X_train = train_df[features].to_numpy()
 y_train = train_df["label"].to_numpy()
-
 X_test = test_df[features].to_numpy()
 y_test = test_df["label"].to_numpy()
 
-# ============================================================
-# Weighted sampler to address class imbalance
-# ============================================================
 class_sample_counts = np.bincount(y_train)
+
 weights = 1.0 / class_sample_counts
 sample_weights = weights[y_train]
 
@@ -168,6 +123,8 @@ sampler = WeightedRandomSampler(
     replacement=True
 )
 
+# dataloaders
+
 train_ds = TensorDataset(
     torch.tensor(X_train, dtype=torch.float32),
     torch.tensor(y_train, dtype=torch.long)
@@ -176,7 +133,6 @@ train_ds = TensorDataset(
 train_loader = DataLoader(
     train_ds,
     batch_size=WINDOW_SEC,
-    #shuffle=True,
     sampler=sampler
 )
 
@@ -188,18 +144,18 @@ test_ds = TensorDataset(
 test_loader = DataLoader(
     test_ds,
     batch_size=WINDOW_SEC,
-    shuffle=False 
+    shuffle=False
 )
 
-# ============================================================
-# Model
-# ============================================================
-# Compute class counts
+
 classes, counts = np.unique(y_train, return_counts=True)
-# Inverse frequency weighting
+
 weights = counts.sum() / (len(classes) * counts)
 class_weights = torch.tensor(weights, dtype=torch.float32)
+
 print("Class weights:", class_weights)
+
+# model
 
 class TransitionMLP(nn.Module):
     def __init__(self, in_dim):
@@ -211,54 +167,55 @@ class TransitionMLP(nn.Module):
             nn.Linear(64, 32),
             nn.ReLU(),
             nn.Dropout(0.1),
-            nn.Linear(32, 2),  # raw logits
+            nn.Linear(32, 2),
         )
 
     def forward(self, x):
         return self.net(x)
 
 model = TransitionMLP(X_train.shape[1])
-opt = torch.optim.Adam(model.parameters(), lr=LR)
-pos_weight = 6.0  # tweak between 3-10
-#loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([1.0, pos_weight]))
-loss_fn = nn.CrossEntropyLoss()#(weight=class_weights)
 
-# ============================================================
-# Train
-# ============================================================
+opt = torch.optim.Adam(model.parameters(), lr=LR)
+
+loss_fn = nn.CrossEntropyLoss()
+
+# training loop
 
 train_losses = []
+
 for epoch in range(EPOCHS):
     epoch_loss = 0.0
+
     for X_batch, y_batch in train_loader:
-        # 1️⃣ Forward pass
-        outputs = model(X_batch)          # raw logits
-        loss = loss_fn(outputs, y_batch) # compute loss
+        outputs = model(X_batch)
+        loss = loss_fn(outputs, y_batch)
 
-        # 2️⃣ Backward pass
-        opt.zero_grad()   # clear previous gradients
-        loss.backward()   # compute gradients w.r.t. weights
-
-        # 3️⃣ Update weights
+        opt.zero_grad()
+        loss.backward()
         opt.step()
 
-        epoch_loss += loss.item() * X_batch.size(0)  # multiply by batch size
+        epoch_loss += loss.item() * X_batch.size(0)
 
-    # Average loss over all samples in epoch
     avg_loss = epoch_loss / len(train_loader.dataset)
     train_losses.append(avg_loss)
-    
+
     print(f"Epoch {epoch+1}/{EPOCHS}, Loss: {avg_loss:.4f}")
 
+# evaluation / testing
+
 model.eval()
+
 with torch.no_grad():
     X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
     test_output = model(X_test_tensor)
+
     test_pred = torch.argmax(test_output, dim=1)
 
     y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
     accuracy = (test_pred == y_test_tensor).float().mean()
 
 print(f"Test Accuracy: {accuracy.item() * 100:.2f}%")
+
 print(confusion_matrix(y_test, test_pred, labels=[0,1]))
 print(classification_report(y_test, test_pred, labels=[0,1]))
