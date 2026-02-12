6ff556d42ee0af8baf88396f116092273cfb28db
Jackson Friday <jackbfriday@gmail.com>
2026-02-11 17:24:08 -0800

    gl shane

--- ML/nngen_out/ml_tb.py
+++ ML/nngen_out/ml_tb.py
@@ -2,7 +2,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.types import LogicArray
 from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge, with_timeout
-from cocotbext.axi import AxiBus, AxiMaster
+from cocotbext.axi import AxiLiteBus, AxiLiteMaster
 from cocotb.triggers import Timer
 
 
@@ -21,7 +21,7 @@ async def reset_test(dut):
     cocotb.start_soon(c.start(start_high=False))
     
     #125 reset sequence
-    active_level = 1 # I think the model.v is active high
+    active_level = 0
     await FallingEdge(clk_i)
     reset_i.value = active_level
 
@@ -48,7 +48,7 @@ async def axis_interface_test(dut):
     cocotb.start_soon(c.start(start_high=False))
     
     #125 reset sequence
-    active_level = 1 # I think the model.v is active high
+    active_level = 0
     await FallingEdge(clk_i)
     reset_i.value = active_level
 
@@ -61,6 +61,13 @@ async def axis_interface_test(dut):
     reset_i._log.debug("Reset complete")
     print(f"irq is {irq}") #what is this???
     
-    axi_master = AxiMaster(AxiBus.from_prefix(dut, "s_axi"), dut.CLK, dut.RESETN)
-    data = await axi_master.read(0x0048, 4) #random test read
+    axi_master = AxiLiteMaster(
+        AxiLiteBus.from_prefix(dut, "saxi"),
+        dut.CLK,
+        dut.RESETN,
+        #reset_active_level=False  # because RESETN is active low
+    )
+    data = await axi_master.write(0x0000, b'test') #random test read
+    await ClockCycles(clk_i, 20)
+    data = await axi_master.read(0x0000, 4) #random test read
     print(f"Read data: {data}")
